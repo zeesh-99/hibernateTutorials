caa3ebce183e2dce871f39bee97dacb0daa215df
Zee <zeesh_99@yahoo.com>
2016-10-31 12:37:56 +0000

    First commit for hibernate tutorial

---
+++ hibernate-tutorial1/src/com/luv2code/hibernate/demo/CreateStudentDemo.java
@@ -0,0 +1,48 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class CreateStudentDemo {
+
+	public static void main(String[] args) {
+		//create session factory
+		
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Student.class)
+									.buildSessionFactory();
+		
+		//create session
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			//use the session object to save Java object
+			
+			// Create a student Object
+			System.out.println("Create new student object...");
+			Student tempStudent = new Student("Paul","Wall","paul@luv2code.com");
+			
+			//Start a transaction
+			session.beginTransaction();
+			
+			//save the student object
+			System.out.println("Savings the student...");
+			session.save(tempStudent);
+			
+			//commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done...");
+			
+		}finally{
+			
+			factory.close();
+			
+		}
+	}
+}
